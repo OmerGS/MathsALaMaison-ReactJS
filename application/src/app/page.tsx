ab13fc13f445faec9c4bfe3ca5737bf5dbcc955f
OmerGS <27omerf@gmail.com>
2025-03-16 14:30:39 +0100

    Create React Project.

---
+++ application/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <p>Bonjour !</p>
+      <button onClick={() => router.push('home')}>Aller vers /home</button>
+    </div>
+  );
+}
