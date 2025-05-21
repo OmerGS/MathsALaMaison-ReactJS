9b81ca05637bf8d6dd6d248493f46b519661aa52
OmerGS <27omerf@gmail.com>
2025-05-21 20:27:12 +0200

    add/user details in home page.

--- application/src/app/home/page.tsx
+++ application/src/app/home/page.tsx
@@ -14,5 +14,17 @@ export default function Homepage() {
 function DashboardContent() {
   const { user } = useUser();
 
-  return <h1>Bienvenue sur ton Dashboard, {user?.pseudo}</h1>;
+  return (
+    <div>
+        <h1>Bienvenue sur ton Dashboard, {user?.pseudo}</h1>
+        <p>votre email : {user?.email}</p>
+        <p>Votre photo de profil : {user?.photoDeProfil}</p>
+        <p>votre nombre de partie : {user?.nombrePartie}</p>
+        <p>votre nombre de victoire : {user?.nombreVictoire}</p>
+        <p>vos points : {user?.point}</p>
+        <p>Votre premium : {user?.isPremium}</p>
+        <p>Votre password : {user?.password}</p>
+        <p>vos salt : {user?.salt}</p>
+    </div>
+  );
 }
